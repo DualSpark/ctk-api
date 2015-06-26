080ae34e59b7dca90b57d433a16121a9952bb5b5
Patrick McClory <patrick@dualspark.com>
2015-06-26 09:07:30 -0700

    updating from deo on 25jun2015

--- buildprocess.sh
+++ buildprocess.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-while read -r line
-do
-    if [[ $line = *amazon-ebs,artifact,0,id* ]]
-    then
-        echo "${line##*,}" | sed 's/%!(PACKER_COMMA)/,/g' | tee -a imagedetails.txt
-    fi
-done < "imagebuild.output.txt"
-
-
-#!/bin/bash
 line=$(grep 'amazon-ebs,artifact,0,id' imagebuild.output.txt)
 image_details=`echo "${line##*,}" | sed 's/%!(PACKER_COMMA)/,/g'`
-aws dynamodb put-item --table-name ctk-api-deploydata --item '{"tier_name":{"S":"'$1'"},"timestamp":{"N":'$(date +"%s")'}, "image_details": {"S": "'$image_details'"}}'
+aws dynamodb put-item --table-name ctk-api-deploydata --item '{"tier_name":{"S":"'$1'"},"timestamp":{"N":"'$(date +"%s")'"}, "image_details": {"S": "'$image_details'"}}'
